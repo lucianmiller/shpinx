50ab619bd40064bf7f64d6f9a9cef7976a7ac40d
Lucian Miller <lucian.miller185@gmail.com>
2023-10-12 11:48:41 -0700

    Update UI logic for second riddle

--- Sphinx/Program.cs
+++ Sphinx/Program.cs
@@ -11,8 +11,14 @@ namespace Sphinx
       Console.WriteLine("I'm the sphinx. If you answer my riddles, you may pass. If you don't, I will eat you!");
       Console.WriteLine("First Riddle: What has one voice but goes on four legs in the morning, two in the afternoon, and three in the evening?");
       string userAnswer1 = Console.ReadLine();
-      Riddle riddle = new Riddle(userAnswer1);
-      Console.WriteLine(riddle.CheckAnswers());
+      Riddle riddle = new Riddle();
+      Console.WriteLine(riddle.CheckAnswers(userAnswer1, 1));
+      if (riddle.ContinueGame)
+      {
+        Console.WriteLine("Second Riddle: The more you take, the more you leave behind. What am I?");
+        string userAnswer2 = Console.ReadLine();
+        Console.WriteLine(riddle.CheckAnswers(userAnswer2, 2));
+      }
     }
   }
 }
