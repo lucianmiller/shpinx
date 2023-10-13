0c2a538770ecdd11e607373aa9d3ac0f846c3d46
Lucian Miller <lucian.miller185@gmail.com>
2023-10-13 10:50:43 -0700

    Add third riddle

--- Sphinx/Models/Riddle.cs
+++ Sphinx/Models/Riddle.cs
@@ -5,17 +5,23 @@ namespace Sphinx.Models
   public class Riddle
   {
     public List<string> Answers { get; set; }
+    public List<string> Questions { get; set; }
     public bool ContinueGame { get; set; }
 
     public Riddle()
     {
-      Answers = new List<string> {"man", "footsteps"};
+      Answers = new List<string> {"man", "footsteps", "sponge"};
+      Questions = new List<string> {
+        "First Riddle: What has one voice but goes on four legs in the morning, two in the afternoon, and three in the evening?",
+        "Second Riddle: The more you take, the more you leave behind. What am I?",
+        "Third Riddle: What is full of holes but still holds water?"
+      };
       ContinueGame = true;
     }
 
     public string CheckAnswers(string userAnswer, int questionNum)
     {
-      if (Answers[questionNum-1].ToLower() == userAnswer)
+      if (Answers[questionNum].ToLower() == userAnswer)
       {
         return "Correct!";
       }
